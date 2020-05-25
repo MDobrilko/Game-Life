e1988ff36f229196c7ad11ddd835f67379afdfc8
Михаил Добрилко <dobmisha@mail.ru>
2020-05-25 17:13:12 +0200

    set one color to all cells

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 // go build -ldflags -H=windowsgui
 
 import (
-	"image/color"
 	"math/rand"
 
 	"golang.org/x/image/colornames"
@@ -24,13 +23,13 @@ func init() {
 	rand.Seed(1000)
 }
 
-func generateFieldOfCells() [][]*color.RGBA {
+func generateFieldOfCells() [][]bool {
 	NCellsX := int64(ScreenWidth / defaultCell.width)
 	NCellsY := int64(ScreenHeight / defaultCell.height)
 
-	field := make([][]*color.RGBA, NCellsX)
+	field := make([][]bool, NCellsX)
 	for i := range field {
-		field[i] = make([]*color.RGBA, NCellsY)
+		field[i] = make([]bool, NCellsY)
 	}
 
 	NCells := NCellsX * NCellsY
@@ -43,81 +42,43 @@ func generateFieldOfCells() [][]*color.RGBA {
 		x := newCell % NCellsX
 		y := newCell / NCellsX
 
-		field[x][y] = &colors[rand.Intn(len(colors))]
+		field[x][y] = true
 	}
 	return field
 }
 
-func mixColor(otherColors []*color.RGBA) *color.RGBA {
-	var mixedColor *color.RGBA = &color.RGBA{0, 0, 0, 255}
-	var count uint8
-	for _, col := range otherColors {
-		if col != nil {
-			mixedColor.R += col.R
-			mixedColor.G += col.G
-			mixedColor.B += col.B
-			count++
-		}
-	}
-
-	var minVal = uint8(40)
-	// var maxVal = uint8(255)
-	if mixedColor.B > minVal && mixedColor.R > 0 && mixedColor.B > 0 && count > 0 {
-		mixedColor.B /= count
-		mixedColor.G /= count
-		mixedColor.R /= count
-	}
-
-	return mixedColor
-}
-
-func isOnField(field [][]*color.RGBA, x, y int) bool {
+func isOnField(field [][]bool, x, y int) bool {
 	return x >= 0 && y >= 0 && x < len(field) && y < len(field[x])
 }
 
-func update(field [][]*color.RGBA, newField [][]*color.RGBA) {
-	var nearbyCells [len(moveX)]*color.RGBA
-	var shadedNearbyCells = nearbyCells[:0]
-
+func update(field [][]bool, newField [][]bool) {
 	for x, col := range field {
-		for y, col := range col {
+		for y, isAlive := range col {
 			counter := 0
 
 			for i := 0; i < len(moveX); i++ {
-				if isOnField(field, x+moveX[i], y+moveY[i]) && field[x+moveX[i]][y+moveY[i]] != nil {
-					shadedNearbyCells = append(shadedNearbyCells, field[x+moveX[i]][y+moveY[i]])
+				if isOnField(field, x+moveX[i], y+moveY[i]) && field[x+moveX[i]][y+moveY[i]] == true {
 					counter++
 				}
 			}
 
-			if counter == 3 || counter == 2 && col != nil {
-				if len(shadedNearbyCells) > 0 {
-					newField[x][y] = mixColor(shadedNearbyCells)
-				} else {
-					newField[x][y] = &colors[rand.Intn(len(colors))]
-				}
+			if counter == 3 || counter == 2 && isAlive {
+				newField[x][y] = true
 			} else {
-				newField[x][y] = nil
+				newField[x][y] = false
 			}
 		}
 	}
 }
 
-func addCells(newField [][]*color.RGBA, field [][]*color.RGBA) {
+func addCells(newField [][]bool, field [][]bool) {
 	for x, column := range field {
-		for y, color := range column {
-			if color == nil && newField[x][y] != nil {
+		for y, isAlive := range column {
+			if isAlive {
 				addDefaultCell(pixel.V(
 					float64(x)*defaultCell.width,
 					float64(y)*defaultCell.height,
 				))
-			} else if color != nil && newField[x][y] != nil {
-				addDefaultCell(pixel.V(
-					float64(x)*defaultCell.width,
-					float64(y)*defaultCell.height,
-				),
-				//field[x][y],
-				)
 			}
 		}
 	}
