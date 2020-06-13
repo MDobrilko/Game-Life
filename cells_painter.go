b672d67c0caa33eb4440d454ac1c57b826fb56db
Михаил Добрилко <dobmisha@mail.ru>
2020-06-13 21:54:10 +0200

    added default empty cell

--- cells_painter.go
+++ cells_painter.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 
 	"github.com/faiface/pixel/pixelgl"
-	"golang.org/x/image/colornames"
 	_ "golang.org/x/image/colornames"
 
 	"github.com/faiface/pixel"
@@ -22,7 +21,7 @@ var defaultCell = cell{10, 10, pixel.RGB(0, 1, 0)}
 var imd = imdraw.New(nil)
 
 func addCell(vertex pixel.Vec, width, height float64, color color.Color) {
-	imd.Color = colornames.White
+	/*imd.Color = colornames.White
 	imd.Push(vertex)
 	imd.Color = colornames.White
 	imd.Push(pixel.V(vertex.X, vertex.Y+height))
@@ -30,7 +29,7 @@ func addCell(vertex pixel.Vec, width, height float64, color color.Color) {
 	imd.Push(pixel.V(vertex.X+width, vertex.Y+height))
 	imd.Color = colornames.White
 	imd.Push(pixel.V(vertex.X+width, vertex.Y))
-	imd.Polygon(1)
+	imd.Polygon(1)*/
 
 	imd.Color = color
 	imd.Push(vertex)
@@ -41,6 +40,18 @@ func addCell(vertex pixel.Vec, width, height float64, color color.Color) {
 	imd.Color = color
 	imd.Push(pixel.V(vertex.X+width, vertex.Y))
 	imd.Polygon(0)
+}
+
+func addDefaultEmptyCell(vertex pixel.Vec) {
+	imd.Color = pixel.RGB(0.1, 0.1, 0.1)
+	imd.Push(vertex)
+	imd.Color = pixel.RGB(0.1, 0.1, 0.1)
+	imd.Push(pixel.V(vertex.X, vertex.Y+defaultCell.height))
+	imd.Color = pixel.RGB(0.1, 0.1, 0.1)
+	imd.Push(pixel.V(vertex.X+defaultCell.width, vertex.Y+defaultCell.height))
+	imd.Color = pixel.RGB(0.1, 0.1, 0.1)
+	imd.Push(pixel.V(vertex.X+defaultCell.width, vertex.Y))
+	imd.Polygon(1)
 }
 
 func addDefaultCell(vertex pixel.Vec) {
